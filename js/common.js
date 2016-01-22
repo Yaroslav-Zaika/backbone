39ac659329874b5b222e52b349fbbddc532bfefb
Yaroslav_Zaika <yaroslav.zaika@gmail.com>
2016-01-22 22:31:38 +0200

    update

---
+++ js/common.js
@@ -0,0 +1,10 @@
+define(function(){
+var config = {
+	apiKey: "2015575636.c567723.f5b88fa304064c8eb896e79003769913", 
+	clientID: "c567723b5690477392e6061dca0634b2", 
+	apiHost: "https://api.instagram.com/",
+	count: 3
+	};
+	return config;
+});
+
